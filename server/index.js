72a272d44785f21fc2ccbd28686b1cd06befde44
Gabriel Clemente <clementeaquipresente@yahoo.com>
2022-07-03 01:23:27 +0200

    checking .env and .gitignore

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js';
+const app = express();
+const PORT = process.env.PORT || 1620;
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+
+app.use(cors());
+app.use('/posts', postRoutes);
+
+mongoose.connect(process.env.CONNECT_DATABASE).then(() => app.listen(PORT, () => (console.info(`Succesfully connected to the database...`),console.info(`Server running on port: ${PORT}...`))))
+    .catch((err) => console.error("THIS IS THE DATABASE CONNECTION ERROR:",err.message));
---
+++ server/routes/posts.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { getPosts, getPost, createPost, updatePost, likePost, deletePost } from '../controllers/PostController.js';
+const router = express.Router();
+
+router.get('/', getPosts);
+router.post('/', createPost);
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
+export default router;
