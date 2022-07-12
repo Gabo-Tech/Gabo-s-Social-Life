74607e19c4c18b03610e6da96dc9719768c9b243
Gabriel Clemente <clementeaquipresente@yahoo.com>
2022-07-12 13:29:20 +0200

    backend

--- client/src/components/CreatorOrTag/CreatorOrTagStyles.js
+++
@@ -1,8 +0,0 @@
-import { makeStyles } from '@material-ui/core/styles';
-
-export default makeStyles((theme) => ({
-  text: {
-    color:"#fff",
-    marginLeft: "3rem"
-  }
-}));
--- client/src/components/Guards/AdminZone.jsx
+++
@@ -1,6 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export default function AdminZone({ children }){
-  const user = JSON.parse(localStorage.getItem("profile"));
-  return user?.user.role === 'admin' ? children : <Navigate to="/" />;
-};
